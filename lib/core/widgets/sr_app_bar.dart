4d6dd642b34e55a054d50f92aebd1d44eb4469d4
Prothes Barai <aspprothesbarai1619@gmail.com>
2026-05-25 10:39:08 +0600

    v2.5.6

--- lib/core/widgets/sr_app_bar.dart
+++ lib/core/widgets/sr_app_bar.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:smartreminder/core/utils/app_colors.dart';
 
-class SrAppBar extends StatelessWidget implements PreferredSizeWidget{
+class SrAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String? appBarTitle;
-  const SrAppBar({super.key, this.appBarTitle});
+  final List<Widget>? actions;
+  final Widget? titleWidget;
+
+  const SrAppBar({super.key, this.appBarTitle, this.actions, this.titleWidget,});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Theme.of(context).colorScheme.surface,
       elevation: 0,
-      title: Text(appBarTitle ?? ""),
       iconTheme: const IconThemeData(color: AppColors.primaryColor),
+      title: titleWidget ?? Text(appBarTitle ?? "", style: const TextStyle(fontWeight: FontWeight.w600),),
+      actions: actions,
     );
   }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:smartreminder/core/widgets/sr_bottom_navigation_bar.dart';
 import 'package:smartreminder/core/widgets/sr_drawer.dart';
 import '../core/config/config.dart';
 import '../core/widgets/exit_app_alert_dialogue.dart';
+import '../core/widgets/sr_app_bar.dart';
 import '../features/reminder_generate/screens/reminder_screen.dart';
 import '../features/schedule_suggest/screens/schedule_screen.dart';
 
@@ -19,6 +20,18 @@ class _HomeScreenState extends State<HomeScreen> {
   final screens = const [ReminderScreen(),ScheduleScreen()];
   void onTabChange(int index){setState(() {currentIndex = index;});}
 
+  // >>> Every Page AppBar Title and Icon ======================================
+  SrAppBar buildAppBar() {
+    switch (currentIndex) {
+      case 0:
+        return const SrAppBar(appBarTitle: "Reminder", actions: [Icon(Icons.add_alert), SizedBox(width: 10),],);
+      case 1:
+        return const SrAppBar(appBarTitle: "Schedule", actions: [Icon(Icons.calendar_month), SizedBox(width: 10),],);
+      default:
+        return const SrAppBar(appBarTitle: "Smart Reminder");
+    }
+  }
+  // <<< Every Page AppBar Title and Icon ======================================
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
       canPop: Config.app == "Android" ? false : true,
       onPopInvokedWithResult: (didPop, dynamic) {  if (didPop) {return;}  BasicAlertDialogue.willPopScope(context);},
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: buildAppBar(),
         drawer: const SrDrawer(),
         body: screens[currentIndex],
         bottomNavigationBar: SrBottomNavigationBar(currentIndex: currentIndex, onTap: onTabChange),
